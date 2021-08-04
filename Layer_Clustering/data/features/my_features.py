478bb30ef613b524bc0c044e11fc87a69c76c906
Aravind R <45995781+AravindR7@users.noreply.github.com>
2021-08-04 21:44:48 +0530

    Delete Layer_Clustering directory

--- Layer_Clustering/data/features/my_features.py
+++
@@ -1,9 +0,0 @@
-from typing import Any
-from layer import Dataset
-
-
-def build_feature(sdf: Dataset("customers")) -> Any:
-
-    data = sdf.to_pandas()
-    input_data = data[['SpendingScore', 'Age']]
-    return input_data
